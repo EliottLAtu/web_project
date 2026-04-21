5f0784730576e95df7454c3b002eca40422ece49
= <eliott.lapicque@epita.fr>
2026-04-21 17:07:07 +0100

    init

---
+++ src/app/favs/favs.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-favs',
+  imports: [],
+  templateUrl: './favs.html',
+  styleUrl: './favs.css',
+})
+export class Favs {
+
+}
